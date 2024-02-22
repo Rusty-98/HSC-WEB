cd7f85b6292d2f11c38107a32c6a41d031364e6c
web_dev <sukritprakash2020@gmail,com>
2024-02-22 13:50:06 +0530

    team added

---
+++ src/Components/Hover/Hover.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './Hover.scss'
+import  img1 from '../Team/giga.jpg'
+
+const Hover = (props) => {
+  return (
+    <div>
+      
+       <div  className="card__collection clear-fix">
+<div className="cards cards--two">
+    
+    <img src={img1} className="img-responsive" alt="Cards "/>
+    <span className="cards--two__rect">
+ 
+    </span>
+    <span className="cards--two__tri"></span>
+    <p>{props.name}<br/></p>
+    
+    <p><br/>{props.role}</p>
+    <ul className="cards__list">
+      <li><i className="fab fa-facebook-f"></i></li>
+      
+      <li><i className="fab fa-instagram"></i></li>
+      <li><i className="fab fa-linkedin-in"></i></li>
+    </ul>
+  </div>
+  
+</div>
+    </div> 
+  )
+}
+
+export default Hover
